d8890785747463bb7303453dfcbdf0f9a918f52e
Peters <73057813+Agla666@users.noreply.github.com>
2021-06-15 15:26:44 +0100

    Initial Commit

---
+++ day1.py
@@ -0,0 +1,130 @@
+#print ("Hello world.")
+
+#for i in range(10):
+    #print (i,i*i)
+
+#num1 = 100
+#num = 500
+
+#print(num1 + num2)
+
+#this is function 
+#print("Blackeye Beans & Rice with lamb and i like it served hot")
+
+# this is a comment
+# your code editor will not interpret these bits because of the "#" symbol
+# for strings we use "quotation marks"
+#var1 = "this is an example of a string"
+# for integers we just use whole numbers
+#var2 = 788
+# for floating points we use decimal places
+#var3 = 3.14152
+# for Booleans it's just True or False
+#var4 = False
+# for None we use nothing or our editor will return "None" when we havent computed something correctly var5 = None
+
+#print(len("hello"))
+
+#print("hello" [2])
+
+#print("THIS IS A STRING YO".lower())
+
+#print("MANCHESTER IS A REAL FUN CITY FOR IT".lower())
+#print("manchester is a real fun city for it".upper())
+
+#import random
+
+#print(random.random())
+
+#print( random.uniform(1,10))
+
+#print(random.randint(1,10))
+
+#print(random.randint(-10,1))
+
+#from random import random, randint, uniform
+
+#Activity me
+
+#print("    |    |    ")
+#print("    |    |    ")
+#print("    |    |    ")
+#print("--------------")
+#print("    |    |    ")
+#print("    |    |    ")
+#print("    |    |    ")
+#print("--------------")
+#print("    |    |    ")
+#print("    |    |    ")
+#print("    |    |    ")
+
+#Alternative 1
+
+#for i in range(3):
+    #print("   |   |   ")
+#for i in range(2):
+    #print("-----------")
+    #for i in range(3):
+        #print("   |   |   ")
+
+
+#Alternative 2
+
+#a = "         \n" 
+#b = "  |  |   \n" 
+#c = "---------\n"
+#d = "  |  |   \n"
+#e = "---------\n"
+#f = "  |  |   \n"
+p#rint(a,b,c,d,e,f) 
+
+#Alternative 3
+
+#a = "|"
+#b = " "
+#c = "-"
+#print(b *3, a, b*3, a)
+#print(b *3, a, b*3, a)
+#print(b *3, a, b*3, a)
+#print(c *15)
+#print(b *3, a, b*3, a)
+#print(b *3, a, b*3, a)
+#print(b *3, a, b*3, a)
+#print(c *15)
+#print(b *3, a, b*3, a)
+#print(b *3, a, b*3, a)
+#print(b *3, a, b*3, a)
+
+#Alternative 4
+
+#print("    |   |    ")
+#print("    |   |    ")
+#print("    |   |    ")
+#print(" ----------- ")
+#print("    |   |    ")
+#print("    |   |    ")
+#print("    |   |    ")
+#print(" ----------- ")
+#print("    |   |    ")
+#print("    |   |    ")
+#print("    |   |    ")
+
+#Activity2
+
+var1 = "This is a string, I use it for my examples."
+print(var1.upper())  # this makes it uppercase
+print(var1.lower())  # this makes things lowercase
+# this capitalises the first letter and lower cases the rest
+print(var1.capitalize())
+print(var1.count("s"))  # this counts how many times something appears in the code
+print(var1.find("string"))  # this finds the index of where something is
+print(var1.replace("examples", "coding"))  # replace one part with another
+print(var1.strip(" "))  # strips away whatever you put in the quote from the front
+
+#Activity3
+
+
+
+
+
+
---
+++ day2.py
@@ -0,0 +1,53 @@
+#print("All around the world"[7].upper())
+
+#variables_one + "apple, carrots, and turnips"
+
+#print(variables_one)
+
+#my_name = "jefferson"
+
+#n = "jefferson"
+
+#print(n)
+
+#my_name = "Jefferson"
+
+#my_age = 69
+
+#student = True
+
+#fave_drink = "Apple juice"
+
+#fave_game = "F1"
+
+#fave_driver = "L Hamilton"
+
+#print("My My Name is {}".format(my_name))
+
+
+#breakfast = "beans" #variable is called breakfast
+#lunch = "rice"
+#dinner = "black eye beans"
+
+
+
+
+print("   |   |   ")
+print(f" {X} | {O}")
+print("   |   |  ")
+print(" ---------")
+print("   |   |  ")
+print(f" {X} | {X} | ")
+print("   |   |  ")
+print(" -----------")
+print("     |   |  ")
+print(f" {O}|   |  ")
+print("     |   |  ")
+
+
+
+
+
+
+
+
